1289f4d50dbcef54c81a9af92c9a6507d57b29b6
franciscocontreras93 <franciscocontreras93@gmail.com>
2025-10-01 04:26:15 -0400

    se actualiza el queri csv_report_data.sql para incluir NO3 y NH4 en los informes.

--- sql/csv_report_data.sql
+++ sql/csv_report_data.sql
@@ -5,7 +5,7 @@ segmento as (select m.idlote,m.codigo,s.segmento,s.ceap from agrae.segmentos s
 -- BUSQUEDA DE LAS MUESTRAS QUE YA EXISTAN PARA LA CAMPA;A  Y QUE LA GREGUE AL REPORTE QUE SE GENERA
 analisis as (select a.* from muestra m left join analytic.analitica a on m.codigo = a.cod),
 unidos as (
-	select m.idlote,m.codigo, round(s.ceap::numeric,2) ceap, a.n, a.p,a.k,a.ph,a.ce,a.carbon,a.ca,a.mg,a.na,a.cic,a.ca_eq,a.mg_eq,a.k_eq,a.na_eq,a.ca_f,a.mg_f,a.k_f,a.na_f,a.s,a.mn,a.b,a.mo,a.zn,a.organi,a.cox,a.rel_cn,a.caliza,a.al,a.fe,a.cu,a.arcilla,a.limo,a.arena,a.ni,a.co,a.ti,a."as",a.pb,a.cr,a.metodo
+	select m.idlote,m.codigo, round(s.ceap::numeric,2) ceap, a.n, a.p,a.k,a.ph,a.ce,a.carbon,a.ca,a.mg,a.na,a.cic,a.ca_eq,a.mg_eq,a.k_eq,a.na_eq,a.ca_f,a.mg_f,a.k_f,a.na_f,a.s,a.mn,a.b,a.mo,a.zn,a.organi,a.cox,a.rel_cn,a.caliza,a.al,a.fe,a.cu,a.arcilla,a.limo,a.arena,a.ni,a.co,a.ti,a."as",a.pb,a.cr,a.metodo,a.no3,a.nh4
 	from muestra m 
 	join data d on m.codigo = d.codigo
 	left join segmento s on m.codigo = s.codigo
