d165064d198869c694f3ed263813bae753cb1a16
franciscocontreras93 <franciscocontreras93@gmail.com>
2025-10-28 12:05:32 +0100

    se cambio el orden de las herramientas de gestion de datos

--- dialogs/tools_dockwidget.py
+++ dialogs/tools_dockwidget.py
@@ -582,7 +582,7 @@ class agraeToolsDockwidget(QtWidgets.QDockWidget):
         self.GestionarParametros = QtWidgets.QAction(agraeGUI().getIcon('matraz'),'Gestionar Parametros',self)
         self.GestionarParametros.triggered.connect(self.gestionParametrosDialog)
 
-        actions_data = [self.GestionarExplotaciones,self.GestionarPersonas,self.GestionarAgricultores,self.GestionarDistribuidores,self.GestionarCultivos] # DELETE FROM MASTER
+        actions_data = [self.GestionarPersonas,self.GestionarExplotaciones,self.GestionarAgricultores,self.GestionarDistribuidores,self.GestionarCultivos] # DELETE FROM MASTER
         # actions_data = [self.GestionarExplotaciones,self.GestionarPersonas,self.GestionarAgricultores,self.GestionarDistribuidores,self.GestionarCultivos,self.GestionarParametros]
         
         self.tools.settingsToolsButtons(self.tool_data,actions_data,icon=agraeGUI().getIcon('list-check'),setMainIcon=True)
