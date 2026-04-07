4cbad417b271f5e1eaa629557d3347f3e670d15a
franciscocontreras93 <franciscocontreras93@gmail.com>
2026-04-07 15:57:54 +0200

    se actualiza el metodo para eliminar lotes haciendo el borrado logico por backend

--- dialogs/tools_dockwidget.py
+++ dialogs/tools_dockwidget.py
@@ -558,9 +558,9 @@ class agraeToolsDockwidget(QtWidgets.QDockWidget):
 
 
 
-        if True:
-            self.toolBox.addItem(self.page_facturacion, "Datos de Facturación y Económicos Generales")
-            self.toolBox.setItemIcon(2,agraeGUI().getIcon('explotacion'))
+        # if True:
+        #     self.toolBox.addItem(self.page_facturacion, "Datos de Facturación y Económicos Generales")
+        #     self.toolBox.setItemIcon(2,agraeGUI().getIcon('explotacion'))
 
 
         self.toolBox.addItem(self.page_gee_module, "Modulo de Google Earth Engine")
